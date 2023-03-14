332da7963c6b428aeccadc480c13a28133ad3e50
Rafal Jagoda <rafaljagoda@gmail.com>
2023-03-14 10:29:23 +0100

    [#184693438] reorder methods, convert public ones to private, improve readme

--- kinde_sdk/kinde_api_client.py
+++ kinde_sdk/kinde_api_client.py
@@ -1,9 +1,8 @@
 from enum import Enum
 from typing import Any, Dict, List, Optional
 
-from authlib.integrations.requests_client import OAuth2Session
 import jwt
-
+from authlib.integrations.requests_client import OAuth2Session
 from kinde_sdk.api_client import ApiClient
 from kinde_sdk.exceptions import (
     KindeConfigurationException,
@@ -19,7 +18,6 @@ class GrantType(Enum):
 
 
 class KindeApiClient(ApiClient):
-
     TOKEN_NAMES = ("access_token", "id_token")
 
     def __init__(
@@ -99,61 +97,58 @@ class KindeApiClient(ApiClient):
     def get_register_url(self) -> str:
         return self.registration_url
 
-    def create_org(self) -> str:
-        return self.create_org_url
-
-    def fetch_token(self, authorization_response: Optional[str] = None) -> None:
-        if self.grant_type == GrantType.CLIENT_CREDENTIALS:
-            params = {"grant_type": "client_credentials"}
-            if self.audience:
-                params["audience"] = self.audience
-        else:
-            if authorization_response is None:
-                raise KindeConfigurationException(
-                    '"authorization_response" parameter is required when grant_type is different than CLIENT_CREDENTIALS.'
-                )
-            params = {"authorization_response": authorization_response}
-        if self.grant_type == GrantType.AUTHORIZATION_CODE_WITH_PKCE:
-            params["code_verifier"] = self.code_verifier
-        self.__access_token_obj = self.client.fetch_token(self.token_endpoint, **params)
-        self.configuration.access_token = self.__access_token_obj.get("access_token")
-        self._clear_decoded_tokens()
-
-    def refresh_token(self) -> None:
-        refresh_token = self.__access_token_obj.get("refresh_token")
-        if refresh_token:
-            self.__access_token_obj = self.client.refresh_token(
-                self.token_endpoint,
-                refresh_token=refresh_token,
-            )
-            self.configuration.access_token = self.__access_token_obj.get(
-                "access_token"
-            )
-            self._clear_decoded_tokens()
-
-    def call_api(self, *args, **kwargs) -> Any:
-        self.get_or_refresh_access_token()
-        return super().call_api(*args, **kwargs)
-
-    def get_or_refresh_access_token(self) -> None:
-        if self.grant_type == GrantType.CLIENT_CREDENTIALS:
-            if not self.__access_token_obj or self.__access_token_obj.is_expired():
-                self.fetch_token()
-        else:
-            if not self.__access_token_obj:
-                raise KindeLoginException(
-                    'Please use "get_login_url()" or "get_register_url()" first.'
-                )
-            if self.__access_token_obj.is_expired():
-                self.refresh_token()
-
-    def is_authenticated(self) -> bool:
-        return self.__access_token_obj and not self.__access_token_obj.is_expired()
-
     def logout(self, redirect_to: str) -> str:
         self.__access_token_obj = None
         self.configuration.access_token = None
         return f"{self.logout_endpoint}?redirect={redirect_to}"
+
+    def is_authenticated(self) -> bool:
+        return self.__access_token_obj and not self.__access_token_obj.is_expired()
+
+    def create_org(self) -> str:
+        return self.create_org_url
+
+    def get_claim(self, key: str, token_name: str = "access_token") -> Any:
+        if token_name not in self.TOKEN_NAMES:
+            raise KindeTokenException(
+                f"Please use only tokens from the list: {self.TOKEN_NAMES}"
+            )
+        self._decode_token_if_needed(token_name)
+        return self.__decoded_tokens[token_name].get(key)
+
+    def get_permission(self, permission: str) -> Dict[str, Any]:
+        return {
+            "org_code": self.get_claim("org_code"),
+            "is_granted": permission in self.get_claim("permissions"),
+        }
+
+    def get_permissions(self) -> Dict[str, Any]:
+        return {
+            "org_code": self.get_claim("org_code"),
+            "permissions": self.get_claim("permissions"),
+        }
+
+    def get_organization(self) -> Dict[str, str]:
+        return {
+            "org_code": self.get_claim("org_code"),
+        }
+
+    def get_user_details(self) -> Dict[str, str]:
+        return {
+            "id": self.get_claim("sub", "id_token"),
+            "given_name": self.get_claim("given_name", "id_token"),
+            "family_name": self.get_claim("family_name", "id_token"),
+            "email": self.get_claim("email", "id_token"),
+        }
+
+    def get_user_organizations(self) -> Dict[str, List[str]]:
+        return {
+            "org_codes": self.get_claim("org_codes", "id_token"),
+        }
+
+    def call_api(self, *args, **kwargs) -> Any:
+        self._get_or_refresh_access_token()
+        return super().call_api(*args, **kwargs)
 
     def _clear_decoded_tokens(self) -> None:
         self.__decoded_tokens = {}
@@ -174,40 +169,43 @@ class KindeApiClient(ApiClient):
             else:
                 raise KindeTokenException(f"Token {token_name} doesn't exist.")
 
-    def get_claim(self, key: str, token_name: str = "access_token") -> Any:
-        if token_name not in self.TOKEN_NAMES:
-            raise KindeTokenException(
-                f"Please use only tokens from the list: {self.TOKEN_NAMES}"
+    def _fetch_token(self, authorization_response: Optional[str] = None) -> None:
+        if self.grant_type == GrantType.CLIENT_CREDENTIALS:
+            params = {"grant_type": "client_credentials"}
+            if self.audience:
+                params["audience"] = self.audience
+        else:
+            if authorization_response is None:
+                raise KindeConfigurationException(
+                    '"authorization_response" parameter is required when grant_type is different than CLIENT_CREDENTIALS.'
+                )
+            params = {"authorization_response": authorization_response}
+        if self.grant_type == GrantType.AUTHORIZATION_CODE_WITH_PKCE:
+            params["code_verifier"] = self.code_verifier
+        self.__access_token_obj = self.client.fetch_token(self.token_endpoint, **params)
+        self.configuration.access_token = self.__access_token_obj.get("access_token")
+        self._clear_decoded_tokens()
+
+    def _get_or_refresh_access_token(self) -> None:
+        if self.grant_type == GrantType.CLIENT_CREDENTIALS:
+            if not self.__access_token_obj or self.__access_token_obj.is_expired():
+                self._fetch_token()
+        else:
+            if not self.__access_token_obj:
+                raise KindeLoginException(
+                    'Please use "get_login_url()" or "get_register_url()" first.'
+                )
+            if self.__access_token_obj.is_expired():
+                self._refresh_token()
+
+    def _refresh_token(self) -> None:
+        refresh_token = self.__access_token_obj.get("refresh_token")
+        if refresh_token:
+            self.__access_token_obj = self.client.refresh_token(
+                self.token_endpoint,
+                refresh_token=refresh_token,
             )
-        self._decode_token_if_needed(token_name)
-        return self.__decoded_tokens[token_name].get(key)
-
-    def get_user_details(self) -> Dict[str, str]:
-        return {
-            "id": self.get_claim("sub", "id_token"),
-            "given_name": self.get_claim("given_name", "id_token"),
-            "family_name": self.get_claim("family_name", "id_token"),
-            "email": self.get_claim("email", "id_token"),
-        }
-
-    def get_permissions(self) -> Dict[str, Any]:
-        return {
-            "org_code": self.get_claim("org_code"),
-            "permissions": self.get_claim("permissions"),
-        }
-
-    def get_permission(self, permission: str) -> Dict[str, Any]:
-        return {
-            "org_code": self.get_claim("org_code"),
-            "is_granted": permission in self.get_claim("permissions"),
-        }
-
-    def get_organization(self) -> Dict[str, str]:
-        return {
-            "org_code": self.get_claim("org_code"),
-        }
-
-    def get_user_organizations(self) -> Dict[str, List[str]]:
-        return {
-            "org_codes": self.get_claim("org_codes", "id_token"),
-        }
+            self.configuration.access_token = self.__access_token_obj.get(
+                "access_token"
+            )
+            self._clear_decoded_tokens()

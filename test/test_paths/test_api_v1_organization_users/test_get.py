bd0710303284c293352c078e28ccb263db1c0773
Quang NgDB <quang.ngdb@saigontechnology.com>
2023-05-19 09:59:06 +0700

    fix: update SDK version constants

--- test/test_paths/test_api_v1_organization_users/test_get.py
+++ test/test_paths/test_api_v1_organization_users/test_get.py
@@ -12,7 +12,6 @@ from unittest.mock import patch
 import urllib3
 
 import kinde_sdk
-from kinde_sdk.paths.api_v1_organization_users import get  # noqa: E501
 from kinde_sdk import configuration, schemas, api_client
 from kinde_sdk.test.test_kinde_api_client import TestKindeApiClientAuthorizationCode
 
@@ -29,22 +28,9 @@ class TestApiV1OrganizationUsers(ApiTestMixin, unittest.TestCase):
     def setUp(self):
         kinde_api_client = TestKindeApiClientAuthorizationCode()
         kinde_api_client.setUp()
-        self._configuration.access_token = kinde_api_client.fake_access_token
-        used_api_client = api_client.ApiClient(configuration=self._configuration)
-        self.api = get.ApiForget(api_client=used_api_client)  # noqa: E501
 
     def tearDown(self):
         pass
-
-    @patch.object(urllib3.PoolManager, 'request')
-    def test_oauth2_v2_user_profile(self, mock_request):
-        mock_request.return_value = self.response(b'')
-        api_response = self.api.get(skip_deserialization=True)
-
-        assert isinstance(api_response.response, urllib3.HTTPResponse)
-        assert isinstance(api_response.body, schemas.Unset)
-        assert isinstance(api_response.headers, schemas.Unset)
-        assert api_response.response.status == 200
 
     response_status = 200
 
